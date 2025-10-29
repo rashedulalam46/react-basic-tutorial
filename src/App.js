5593af261c5ebd598a155f48296021f6eee79234
Rashedul Alam <rashed464@outlook.com>
2025-10-29 22:12:18 +0600

    code updated

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import './App.css';
 import Home from './Pages/Home';
 import Header from './Pages/Header';
 import Footer from './Pages/Footer';
+import Contact from './Pages/Contact';
 
 function App() {
   return (
     <div className="App">
       <Header />
       <Home />
+      <Contact />
       <Footer />
     </div>
   );
---
+++ src/Pages/Contact.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Contact() {
+  return (
+    <div>
+      <h2>Contact Us</h2>
+      <p>If you have any questions, feel free to reach out!</p>
+    </div>
+  );
+}
+
+export default Contact;
