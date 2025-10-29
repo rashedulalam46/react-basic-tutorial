92beb44b4f97831ec7d5d670094fca2b74a31b3d
Rashedul Alam <rashed464@outlook.com>
2025-10-29 21:53:35 +0600

    code updated

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import Home from './Pages/Home';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-       <h1>Hello World</h1>
+        <Home />
       </header>
     </div>
   );
