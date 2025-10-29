c34efb7ae07c0b5e6751aeb9141fff2ed795c488
Rashedul Alam <rashed464@outlook.com>
2025-10-29 23:55:42 +0600

    prop example

--- src/App.js
+++ src/App.js
@@ -10,7 +10,8 @@ function App() {
     <div className="App">
       <Header />
       <Home />
-      <Contact />
+      <Contact email="contact1@example.com" phone="123-456-7891" />
+      <Contact email="contact2@example.com" phone="123-456-7892" />
       <Footer />
     </div>
   );
--- src/Pages/Contact.jsx
+++ src/Pages/Contact.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 
-function Contact() {
+function Contact(props) {
   return (
     <div>
       <h2>Contact Us</h2>
       <p>If you have any questions, feel free to reach out!</p>
+      <p>Email: {props.email}</p>
+      <p>Phone: {props.phone}</p>
     </div>
   );
 }
