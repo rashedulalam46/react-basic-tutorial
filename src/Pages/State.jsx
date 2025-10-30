c00c690614cac71e87a11da7ff9500cdc324d818
Rashedul Alam <rashed464@outlook.com>
2025-10-30 11:32:22 +0600

    code changes

--- src/Pages/State.jsx
+++ src/Pages/State.jsx
@@ -7,19 +7,19 @@ const constState = {
     phone: {
         home: "555-1234",
         work: "555-5678"
-    }   
     }
+}
 
 function State() {
-  return (
-    <div>
-      <h1>{constState.title}</h1>
-      <h2>Name: {constState.name}</h2>
-      <p>Email: {constState.email}</p>
-      <p>Home Phone: {constState.phone.home}</p>
-      <p>Work Phone: {constState.phone.work}</p>
-    </div>
-  );
+    return (
+        <div>
+            <h1>{constState.title}</h1>
+            <h2>Name: {constState.name}</h2>
+            <p>Email: {constState.email}</p>
+            <p>Home Phone: {constState.phone.home}</p>
+            <p>Work Phone: {constState.phone.work}</p>
+        </div>
+    );
 }
 
 export default State;
